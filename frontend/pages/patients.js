95552e64e38ea8c0ee3bf56b36c00b206dd522d7
arifoyong <arifoyong@gmail.com>
2022-01-31 12:12:17 +0800

    Frontend: Navbar, Layout

--- frontend/pages/patients.js
+++ frontend/pages/patients.js
@@ -1,32 +1,71 @@
+import {useState} from 'react'
+import Layout from '../components/Layout'
+import Modal from '../components/Modal'
+import { PencilIcon, TrashIcon } from '@heroicons/react/solid'
+
+const formatDate = (strDate) => {
+  const monthNames =["Jan","Feb","Mar","Apr",
+                      "May","Jun","Jul","Aug",
+                      "Sep", "Oct","Nov","Dec"];
+  const dt = new Date(strDate)
+
+  const day = dt.getDate()
+  const mth = monthNames[dt.getMonth()]
+  const yr = dt.getFullYear()
+
+  return `${day}-${mth}-${yr}`
+}
+
+const Patient = ({data}) => {
+  const [showModal, setShowModal] = useState(false)
 
 
-const Patient = ({data}) => (
-  <main className="px-4">
-    <p className="text-3xl py-4">Patient page</p>
-    <table className="table-auto">
-      <thead className="bg-blue-700 text-white">
-        <tr>
-          <td className="px-2 py-2">Firstname</td>
-          <td className="px-2 py-2">Lastname</td>
-          <td className="px-2 py-2">IC</td>
-          <td className="px-2 py-2">DOB</td>
-          <td className="px-2 py-2">Email</td>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((patient, idx) => (
-          <tr key={idx}>
-            <td className="px-2 py-2">{patient.firstname}</td>
-            <td className="px-2 py-2">{patient.lastname}</td>
-            <td className="px-2 py-2">{patient.ic}</td>
-            <td className="px-2 py-2">{patient.dob}</td>
-            <td className="px-2 py-2">{patient.email}</td>
+  return (
+  <Layout>
+    <main className="px-4">
+      <p className="text-3xl py-4">Patient page</p>
+      <p onClick={() => setShowModal(true)}>add</p>
+      <Modal onClose = {() => setShowModal(false) } show={showModal}> 
+        <p>This is from the children</p>
+      </Modal>
+
+      <table className="table-auto w-full">
+        <thead className="bg-blue-700 text-white">
+          <tr>
+            <td className="px-2 py-2">Firstname</td>
+            <td className="px-2 py-2">Lastname</td>
+            <td className="px-2 py-2">IC</td>
+            <td className="px-2 py-2">DOB</td>
+            <td className="px-2 py-2">Email</td>
+            <td className="px-2 py-2">Phone</td>
+            <td className="px-2 py-2">Address</td>
+            <td className="px-2 py-2"></td>
           </tr>
-          ))}
-      </tbody>
-    </table>
-  </main>
-)
+        </thead>
+        <tbody> 
+          {data.map((patient, idx) => (
+            <tr key={idx}>
+              <td className="px-2 py-2">{patient.firstname}</td>
+              <td className="px-2 py-2">{patient.lastname}</td>
+              <td className="px-2 py-2">{patient.ic}</td>
+              <td className="px-2 py-2">{formatDate(patient.dob)}</td>
+              <td className="px-2 py-2">{patient.email}</td>
+              <td className="px-2 py-2">{patient.phone}</td>
+              <td className="px-2 py-2">{patient.address}</td>
+              <td className="px-2 py-2">
+                <div className="flex space-x-1">
+                  <PencilIcon className="w-4 h-4"/>
+                  <TrashIcon className="w-4 h-4"/>
+                </div>
+                </td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
+    </main>
+  </Layout>
+  )
+}
 
 export async function getStaticProps(context) {
   const res = await fetch(`http://localhost:8000/patients`)
